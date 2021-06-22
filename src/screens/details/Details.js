ad5ebef09aaddd6880feef0b773c2aaa8bb7d705
varshat <varshat91@gmail.com>
2021-06-22 15:26:42 +0530

    handler and functionality included

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -41,6 +41,180 @@ class Details extends Component {
             itemAddedFromCart: false,
         };
     }
+    componentDidMount() {
+        // Get profile
+        let data = null;
+        let xhr = new XMLHttpRequest();
+        let that = this;
+
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                that.setState({
+                    id: JSON.parse(this.responseText).id,
+                    restaurant_name: JSON.parse(this.responseText).restaurant_name,
+                    photo_URL: JSON.parse(this.responseText).photo_URL,
+                    customer_rating: JSON.parse(this.responseText).customer_rating,
+                    average_price: JSON.parse(this.responseText).average_price,
+                    number_customers_rated: JSON.parse(this.responseText)
+                        .number_customers_rated,
+                    locality: JSON.parse(this.responseText).address.locality,
+                    categories: JSON.parse(this.responseText).categories,
+                    orderItems: { id: JSON.parse(this.responseText).id },
+                });
+            }
+        });
+
+        let url = this.props.baseUrl + "restaurant/";
+
+        xhr.open("GET", url + this.props.match.params.restaurantId);
+        xhr.setRequestHeader("Cache-Control", "no-cache");
+        xhr.send(data);
+    }
+
+    getIndex = (value, arr, prop) => {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i][prop] === value) {
+                return i;
+            }
+        }
+        return -1; //case where the value doesn't exist
+    };
+
+    addToCartHandler = (e, id, type, name, price) => {
+        var totalAmount = this.state.totalAmount;
+        var totalItems = this.state.totalItems;
+        totalItems += 1;
+
+        const newItem = this.state.cartItem;
+        newItem.id = id;
+        newItem.type = type;
+        newItem.name = name;
+        newItem.pricePerItem = price;
+        newItem.quantity = 1;
+        newItem.priceForAll = price;
+
+        this.setState({ cartItem: newItem });
+
+        totalAmount += price;
+
+        if (
+            this.state.orderItems.items !== undefined &&
+            this.state.orderItems.items.some((item) => item.name === name)
+        ) {
+            var index = this.getIndex(name, this.state.orderItems.items, "name");
+            var quantity = this.state.orderItems.items[index].quantity + 1;
+            var priceForAll =
+                this.state.orderItems.items[index].priceForAll +
+                this.state.orderItems.items[index].pricePerItem;
+            var item = this.state.orderItems.items[index];
+            item.quantity = quantity;
+            item.priceForAll = priceForAll;
+            this.setState(item);
+        } else {
+            this.state.cartItems.push(this.state.cartItem);
+            this.setState({ cartItem: {} });
+
+            const orderItems = this.state.orderItems;
+            orderItems.items = this.state.cartItems;
+            this.setState({ orderItems: orderItems });
+        }
+
+        this.setState({ open: true });
+        this.setState({ totalItems: totalItems });
+        this.setState({ totalAmount: totalAmount });
+    };
+
+    removeFromCartHandler = (e, id, type, name, price) => {
+        var index = this.getIndex(name, this.state.orderItems.items, "name");
+
+        if (this.state.orderItems.items[index].quantity > 1) {
+            var quantity = this.state.orderItems.items[index].quantity - 1;
+            var priceForAll =
+                this.state.orderItems.items[index].priceForAll -
+                this.state.orderItems.items[index].pricePerItem;
+            var item = this.state.orderItems.items[index];
+            item.quantity = quantity;
+            item.priceForAll = priceForAll;
+            this.setState(item);
+            this.setState({ itemQuantityDecreased: true });
+        } else {
+            this.state.orderItems.items.splice(index, 1);
+            this.setState({ itemRemovedFromCart: true });
+        }
+
+        var totalAmount = this.state.totalAmount;
+        totalAmount -= price;
+        var totalItems = this.state.totalItems;
+        totalItems -= 1;
+
+        this.setState({ totalItems: totalItems });
+        this.setState({ totalAmount: totalAmount });
+    };
+
+    addAnItemFromCartHandler = (item, index) => {
+        const itemIndex = this.getIndex(
+            item.name,
+            this.state.orderItems.items,
+            "name"
+        );
+
+        var quantity = this.state.orderItems.items[itemIndex].quantity + 1;
+        var priceForAll =
+            this.state.orderItems.items[itemIndex].priceForAll +
+            this.state.orderItems.items[itemIndex].pricePerItem;
+        var itemAdded = this.state.orderItems.items[itemIndex];
+        itemAdded.quantity = quantity;
+        itemAdded.priceForAll = priceForAll;
+        this.setState(item);
+        this.setState({ itemQuantityIncreased: true });
+        var totalAmount = this.state.totalAmount;
+        totalAmount += item.pricePerItem;
+        var totalItems = this.state.totalItems;
+        totalItems += 1;
+
+        this.setState({ totalItems: totalItems });
+        this.setState({ totalAmount: totalAmount });
+    };
+
+    closeHandler = () => {
+        this.setState({ open: false });
+        this.setState({ cartEmpty: false });
+        this.setState({ nonloggedIn: false });
+        this.setState({ itemQuantityDecreased: false });
+        this.setState({ itemRemovedFromCart: false });
+        this.setState({ itemAddedFromCart: false });
+        this.setState({ itemQuantityIncreased: false });
+    };
+
+    checkoutHandler = () => {
+        if (this.state.totalItems === 0) {
+            this.setState({ cartEmpty: true });
+        } else if (
+            this.state.totalItems > 0 &&
+            sessionStorage.getItem("access-token") === null
+        ) {
+            this.setState({ nonloggedIn: true });
+        } else {
+            this.props.history.push({
+                pathname: "/checkout/",
+                state: {
+                    orderItems: this.state.orderItems,
+                    total: this.state.totalAmount,
+                    restaurantName: this.state.restaurant_name,
+                },
+            });
+        }
+    };
+
+    Capitalize(str) {
+        var arr = str.split(" ");
+        var pascalCasedString = "";
+        arr.map(
+            (a) => (pascalCasedString += a.charAt(0).toUpperCase() + a.slice(1) + " ")
+        );
+        return pascalCasedString;
+    }
+    
 
     render() {
         return (
@@ -53,14 +227,14 @@ class Details extends Component {
                             <img src={this.state.photo_URL} alt="none" className="restaurant-image" />
                         </div>
                         <div className="restaurant-right-container">
-                            <div style={{ fontWeight: "medium", fontSize: "30px", paddingTop: "10px", paddingBottom: "10px"}}> {this.state.restaurant_name}</div>
+                            <div style={{ fontWeight: "medium", fontSize: "30px", paddingTop: "10px", paddingBottom: "10px" }}> {this.state.restaurant_name}</div>
                             <div style={{ fontWeight: "medium", fontSize: "16px", paddingBottom: "10px" }}> {this.state.locality}</div>
                             <div style={{ fontSize: "14px", paddingBottom: "20px" }}>
                                 {this.state.categories.map((category, index) => (
                                     <span key={category.id + "category"}>
                                         {category.category_name}
                                         {index < this.state.categories.length - 1 ? ", " : " "}{" "}
-                                    </span>))} 
+                                    </span>))}
                             </div>
                             <div className="rating-section">
                                 <div className="rating-section-left">
@@ -73,10 +247,10 @@ class Details extends Component {
                                     </i>
                                     {this.state.customer_rating}
                                     <div style={{ color: "gray", fontSize: "12px" }}>AVERAGE RATING BY</div>
-                                    <div style={{ color: "gray", fontSize: "12px"}}> {this.state.number_customers_rated} CUTOMERS</div>
+                                    <div style={{ color: "gray", fontSize: "12px" }}> {this.state.number_customers_rated} CUTOMERS</div>
                                 </div>
                                 <div className="rating-section-right">
-                                    <i className="fa fa-inr" aria-hidden="true" style={{ paddingRight: "4px",  paddingBottom: "3px", paddingLeft: "2px" }}>
+                                    <i className="fa fa-inr" aria-hidden="true" style={{ paddingRight: "4px", paddingBottom: "3px", paddingLeft: "2px" }}>
                                     </i>
                                     {this.state.average_price}
                                     <div style={{ color: "gray", fontSize: "12px" }}>AVERAGE COST FOR</div>
@@ -89,15 +263,15 @@ class Details extends Component {
                         <div className="category-items-container">
                             {this.state.categories.map((category) => (
                                 <div className="category" key={"category" + category.id}>
-                                    <span style={{color: "grey",fontWeight: "bolder"}} >
+                                    <span style={{ color: "grey", fontWeight: "bolder" }} >
                                         {category.category_name.toUpperCase()}
                                     </span>{" "}
-                                    <Divider style={{ marginTop: "10px", marginBottom: "10px", width: "90%"}}/>
+                                    <Divider style={{ marginTop: "10px", marginBottom: "10px", width: "90%" }} />
                                     {category.item_list.map((item) => (
                                         <Grid container key={item.id} style={{ marginBottom: 5 }}>
                                             <Grid item xs={1} lg={1}>
                                                 {item.item_type === "VEG" ? (
-                                                    <span className="fa fa-circle" aria-hidden="true" style={{ fontSize: "12px", color: "green" }}/>) : (
+                                                    <span className="fa fa-circle" aria-hidden="true" style={{ fontSize: "12px", color: "green" }} />) : (
                                                     <span className="fa fa-circle" ria-hidden="true" style={{ fontSize: "12px", color: "red" }} />
                                                 )}
                                             </Grid>
@@ -118,7 +292,15 @@ class Details extends Component {
                                             </Grid>
                                             <Grid item xs={1} lg={1}></Grid>
                                             <Grid item xs={2} lg={2}>
-                                                <IconButton style={{ padding: 0, float: "left" }}>
+                                                <IconButton style={{ padding: 0, float: "left" }}
+                                                    onClick={(e) =>
+                                                        this.addToCartHandler(
+                                                            e,
+                                                            item.id,
+                                                            item.item_type,
+                                                            item.item_name,
+                                                            item.price
+                                                        )}>
                                                     <AddIcon style={{ padding: 0 }} fontSize="small" />
                                                 </IconButton>
                                             </Grid>
@@ -149,14 +331,14 @@ class Details extends Component {
                                                                         fontSize: "12px",
                                                                         color: "green",
                                                                         paddingRight: "12px",
-                                                                    }}/>
+                                                                    }} />
                                                             ) : (
                                                                 <span className="fa fa-stop-circle-o" aria-hidden="true"
                                                                     style={{
                                                                         fontSize: "12px",
                                                                         color: "red",
                                                                         paddingRight: "12px",
-                                                                    }}/>
+                                                                    }} />
                                                             )}
                                                         </Grid>
                                                         <Grid item xs={3} lg={4}>
@@ -166,12 +348,20 @@ class Details extends Component {
                                                         </Grid>
                                                         <Grid item xs={3} lg={3} style={{ flexWrap: "wrap" }}>
                                                             <div className="add-remove-icon">
-                                                                <IconButton className="add-remove-button-hover" style={{ display: "flex", padding: 0 }}>
+                                                                <IconButton className="add-remove-button-hover" style={{ display: "flex", padding: 0 }}
+                                                                    onClick={(e) =>
+                                                                        this.removeFromCartHandler(
+                                                                            e,
+                                                                            item.id,
+                                                                            item.type,
+                                                                            item.name,
+                                                                            item.pricePerItem
+                                                                        )}>
                                                                     <RemoveIcon
                                                                         fontSize="default" style={{
                                                                             color: "black",
                                                                             fontWeight: "bolder",
-                                                                        }}/>
+                                                                        }} />
                                                                 </IconButton>
                                                                 <Typography style={{ fontWeight: "bold" }}>
                                                                     {item.quantity}
@@ -179,13 +369,16 @@ class Details extends Component {
                                                                 <IconButton
                                                                     className="add-remove-button-hover"
                                                                     style={{ display: "flex", padding: 0 }}
-                                                                    >
+                                                                    onClick={this.addAnItemFromCartHandler.bind(
+                                                                        this,
+                                                                        item,
+                                                                        index)}>
                                                                     <AddIcon
                                                                         fontSize="default"
                                                                         style={{
                                                                             color: "black",
                                                                             fontWeight: "bolder",
-                                                                        }}/>
+                                                                        }} />
                                                                 </IconButton>
                                                             </div>
                                                         </Grid>
@@ -219,7 +412,7 @@ class Details extends Component {
                                                 </div>
                                             </Grid>
                                             <Grid item xs={12}>
-                                                <Button className="checkout" variant="contained" color="primary" >
+                                                <Button className="checkout" variant="contained" color="primary" onClick={this.checkoutHandler}>
                                                     <Typography>CHECKOUT</Typography>
                                                 </Button>
                                             </Grid>
@@ -229,6 +422,7 @@ class Details extends Component {
                             </Card>
                         </div>
                     </div>
+                    
                 </div>
             </div>
         );
